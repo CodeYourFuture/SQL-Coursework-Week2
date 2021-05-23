205b74499526d897abe6e927b1ebe455b22d7f09
SenaitEkubai <Senaitekubai@gmail.com>
2021-05-23 09:43:53 +0100

    created nodejs application called cyf-ecommerce-api

---
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { Pool } = require("pg");
+const app = express();
+const dbConfig = {
+  host: "localhost",
+  port: 5432,
+  user: "cyf",
+  password: "CYFStudent123",
+  database: "cyf_ecommerce",
+};
+const pool = new Pool(dbConfig);
+
+// psql queries
+
+const allCustomers = `select name from customers`;
+const allSuppliers = `select supplier_name from suppliers`;
+const allProducts = `select product_name,supplier_name,unit_price from products 
+inner join product_availability on products.id=product_availability.prod_id
+inner join suppliers on product_availability.supp_id=suppliers.id;
+`;
+
+// customers end point
+
+app.get("/customers", (req, res) =>
+  pool
+    .query(allCustomers)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => res.status(500).send(error))
+);
+
+// suppliers end point
+
+app.get("/suppliers", (req, res) =>
+  pool
+    .query(allSuppliers)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => res.status(500).send(error))
+);
+
+// products end point
+
+app.get("/products", (req, res) =>
+  pool
+    .query(allProducts)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => res.status(500).send(error))
+);
+
+app.listen(3000, () => console.log("Running on port 3000"));
