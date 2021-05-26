a87f533e39d06c8c70ed0660aebdced352341a87
Amanuel <weditedi25@gmail.com>
2021-05-26 17:30:33 +0100

    customer end point

--- 3-projects/backend/server.js
+++ 3-projects/backend/server.js
@@ -10,6 +10,19 @@ const dbConfig = {
 };
 const pool = new Pool(dbConfig);
 
+const customerSelectQuery = `SELECT * FROM customers `;
+
+app.get("/", (req, res) => {
+  res.send("first page");
+});
+
+app.get("/customers", (req, res) =>
+  pool
+    .query(customerSelectQuery)
+    .then((result) => res.send(result.rows))
+    .catch((error) => res.status(500).send(error))
+);
+
 app.listen(3000, () => {
   console.log(" Server running on port 3000");
 });
