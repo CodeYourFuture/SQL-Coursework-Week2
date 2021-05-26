762429ad348136c084506d0e73c517b077d72631
Amanuel <weditedi25@gmail.com>
2021-05-26 17:35:14 +0100

    suppliers end point created

--- 3-projects/backend/server.js
+++ 3-projects/backend/server.js
@@ -11,17 +11,29 @@ const dbConfig = {
 const pool = new Pool(dbConfig);
 
 const customerSelectQuery = `SELECT * FROM customers `;
+const suppliersSelectQuery = `SELECT * FROM suppliers `;
 
 app.get("/", (req, res) => {
   res.send("first page");
 });
 
+//customers route
 app.get("/customers", (req, res) =>
   pool
     .query(customerSelectQuery)
     .then((result) => res.send(result.rows))
     .catch((error) => res.status(500).send(error))
 );
+
+//suppliers end point
+app.get("/suppliers", async (req, res) => {
+  try {
+    const result = await pool.query(suppliersSelectQuery);
+    res.send(result.rows);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 app.listen(3000, () => {
   console.log(" Server running on port 3000");
