0c02796b97af4f6ea3bb86c2ada9688701344f0c
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-08 23:40:48 +0200

    loading data from database with a GET endpoint

---
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const pool = new Pool({
+  user: 'douglas',
+  host: 'localhost',
+  database: 'cyf_ecommerce',
+  password: process.env.ADMIN_PASSWORD,
+  port: 5432
+});
+
+app.get('/', (req, res) => {
+  res.send(`I feel like jumping into this POOL!`)
+});
+
+app.get('/customers', (req, res) => {
+  pool.query('SELECT * FROM customers', (error, result) => {
+    res.json(result)
+  });
+});
+
+const listener = app.listen(PORT, () => {
+  console.log(`Server started on port: ${listener.address().port}`)
+});
