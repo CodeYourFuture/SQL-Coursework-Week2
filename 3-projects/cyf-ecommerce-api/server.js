6426884c56040471dec99ea66d0baf78bb55808e
Ryno <rjrfourie@outlook.com>
2021-08-10 00:36:54 +0200

    reinstall everything

---
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "Ryno",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "16139",
+  port: 5432,
+});
+
+app.get("/customers", function (req, res) {
+  pool.query("SELECT * FROM customers", (error, result) => {
+    console.log("found me");
+    res.json(result.rows);
+  });
+});
+
+const listener = app.listen(3000 || process.env.PORT, () => {
+  console.log("Server is listening on port " + listener.address().port);
+});
