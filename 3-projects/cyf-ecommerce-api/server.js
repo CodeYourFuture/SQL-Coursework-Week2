cf5b6fba6b92e6013a6fb3253b439556ff1cadcb
musayuksel <musayuxel@gmail.com>
2021-12-01 21:50:17 +0000

    feat: add /customers endpoint

--- 3-projects/cyf-ecommerce-api/server.js
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "",
+  port: 5432,
+});
+
+app.get("/customers", function (req, res) {
+  pool.query("SELECT * FROM customers", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Port running on ${PORT}`);
 });
