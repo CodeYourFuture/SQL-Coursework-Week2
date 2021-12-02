f362a35c303f509ce66b9676fd7a2249ebc2b34b
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-02 00:40:20 +0000

    feat: add server

---
+++ 3-projects/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "",
+  port: 5432,
+});
+app.get("/", (req, res) => {
+  res.send(
+    `getting ecommerce database`
+  );
+}); 
+app.get("/customers", function (req, res) {
+  pool.query("SELECT * FROM customers", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+app.get("/suppliers", function (req, res) {
+  pool.query("SELECT * FROM suppliers", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+app.get("/products", function (req, res) {
+  pool.query("SELECT products.product_name, product_availability.unit_price, suppliers.supplier_name FROM products INNER JOIN product_availability ON product_availability.prod_id = products.id INNER JOIN suppliers ON product_availability.supp_id = suppliers.id",
+  (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+
+app.listen(PORT, ()=> {
+    console.log(`App is listening at port: ${PORT}`)
+});
