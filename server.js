d32b08a4b490641d21096c82d69bc4c40f88ae76
Texx45 <tcalderbank@hotmail.com>
2021-12-03 06:36:40 +0000

    stretch goal completed - project

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const pool = new Pool({
   port: 5432,
 });
 
-app.get("/suppliers", function (req, res) {
-  pool.query("SELECT * FROM suppliers", (error, result) => {
+app.get("/customers", function (req, res) {
+  pool.query("SELECT * FROM customers", (error, result) => {
     res.json(result.rows);
   });
 });
@@ -21,6 +21,15 @@ app.get("/suppliers", function (req, res) {
   pool.query("SELECT * FROM suppliers", (error, result) => {
     res.json(result.rows);
   });
+});
+
+app.get("/products", (req, res) => {
+  pool.query(
+    "SELECT product_name, unit_price, supplier_name FROM products INNER JOIN products_availability ON products.id =products_availability.prod_id INNER JOIN suppliers ON suppliers.id= products.availability.supp_id;",
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
 });
 
 app.listen(PORT, () => {
