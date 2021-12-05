e9e15d78b71bda953c716fcfd27661729b419647
ali bavarsad <a.bavarsad007@gmail.com>
2021-12-05 23:38:31 +0000

    add /products endpoint

--- 3-projects/cyf-ecommerce-api/server.js
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -20,10 +20,18 @@ app.get("/customers", (req, res) => {
 app.get("/suppliers", (req, res) => {
   pool.query("SELECT * FROM suppliers", (error, resault) => {
     res.json(resault.rows);
-  })
-})
+  });
+});
 
-app.get
+app.get("/products", (req, res) => {
+  const selectQuery = `SELECT product_name, unit_price, supplier_name
+                      FROM products
+                      INNER JOIN product_availability ON product_availability.prod_id = products.id
+                      INNER JOIN suppliers ON product_availability.supp_id  = suppliers.id; `;
+  pool.query(selectQuery, (error, resault) => {
+    res.send(resault.rows);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
