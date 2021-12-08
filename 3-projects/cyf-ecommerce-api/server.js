3d39d25c8de964a0c7e602740a663b289ded69c0
Sharm-Dev <sharmainetaylor@ymail.com>
2021-12-08 20:01:12 +0000

    completed project all 5 tasks

--- 3-projects/cyf-ecommerce-api/server.js
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -1,20 +1,43 @@
-const app = express();
-const PORT = process.env.PORT || 300;
+const express = require("express");
+const app = express(); 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+const PORT = process.env.PORT || 3000;
 
 const { Pool } = require('pg')
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "cyf-ecommerce",
+  database: "cyf_ecommerce",
   password: "",
   port: 5432,
 });
 
 app.get("/customers", function(req, res){
-    pool.query("SELECT * FROM customers", (erorr, result) => {
-        res.json(result.rows)
-    })
+    pool.query("SELECT * FROM customers") 
+        .then((result) => res.json(result.rows))
+        .catch((e)=> console.error(e))
 })
+
+app.get("/suppliers", function (req, res) {
+  pool
+    .query("SELECT * FROM suppliers")
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get("/products", function (req, res) {
+  pool
+    .query(
+      "SELECT products.product_name, product_availability.unit_price, suppliers.supplier_name FROM products INNER JOIN product_availability ON products.id = product_availability.prod_id INNER JOIN suppliers ON suppliers.id = product_availability.supp_id;"
+    )
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
 
 
 app.listen(PORT, function(){
