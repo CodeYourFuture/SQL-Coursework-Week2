9d8b5535804163ad252e99a43ee8adcf3e4a20c0
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-12-10 18:40:56 +0000

    sql week 3

--- 3-projects/cyf-ecommerce-api/server.js
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, function () {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
-});
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
@@ -15,56 +13,47 @@ const pool = new Pool({
 });
 
 app.get("/customers", function (req, res) {
-  pool.query("SELECT * FROM customers", (error, result) => {
-    if (res.status(200)) {
-      res.status(200).json(result.rows);
-    } else {
-      res.status(500).send(error.message);
-    }
-  });
+  pool
+    .query("SELECT * FROM customers")
+    .then((result) => res.status(200).json(result.rows))
+    .catch((e) => console.error(e));
 });
+app.get("/customers/:id", function (req, res) {
+  let id =req.params.id;
+  pool
+    .query("SELECT * FROM customers where customers.id =$1", [Number(id)])
+  .then((result) => res.status(200).json(result.rows))
+    .catch((e) => console.error(e));
+    }
+  );
+
 app.get("/suppliers", function (req, res) {
-  pool.query("SELECT * FROM suppliers", (error, result) => {
-    if (res.status(200)) {
-      res.status(200).json(result.rows);
-    } else {
-      res.status(500).send(error.message);
-    }
+  pool
+    .query("SELECT * FROM suppliers")
+    .then((result) => res.status(200).json(result.rows))
+    .catch((e) => console.error(e));
   });
-});
 
 app.get("/products", function (req, res) {
   let product_name
   if (req.query.name) {
     product_name = req.query.name.toLowerCase();
     pool.query(
-      "select products.product_name, product_availability.unit_price,suppliers.supplier_name from products inner join product_availability on products.id=product_availability.prod_id inner join suppliers on product_availability.supp_id=suppliers.id where LOWER(products.product_name) like '%" + product_name +
-      "%'; ",
-    
-      (error, result) => {
-        if (res.status(200)) {
-          res.status(200).json(result.rows);
-        } else {
-          res.status(500).send(error.message);
-        }
-      }
-    );
-    
+      "select products.product_name, product_availability.unit_price,suppliers.supplier_name from products inner join product_availability on products.id=product_availability.prod_id inner join suppliers on product_availability.supp_id=suppliers.id where LOWER(products.product_name) like $1", ['%' +product_name + '%']).
+      then((result) => res.status(200).json(result.rows)).
+      catch((e) => console.error(e))
   }
-
+      
+    
   else {
 
     pool.query(
-      "select products.product_name, product_availability.unit_price,suppliers.supplier_name from products inner join product_availability on products.id=product_availability.prod_id inner join suppliers on product_availability.supp_id=suppliers.id",
-  
-
-      (error, result) => {
-        if (res.status(200)) {
-          res.status(200).json(result.rows);
-        } else {
-          res.status(500).send(error.message);
-        }
-      }
-    );
-  };
-})
+      "select products.product_name, product_availability.unit_price,suppliers.supplier_name from products inner join product_availability on products.id=product_availability.prod_id inner join suppliers on product_availability.supp_id=suppliers.id").
+      then((result) => res.status(200).json(result.rows)).
+      catch((e) => console.error(e))
+  }
+    
+});
+app.listen(3001, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
