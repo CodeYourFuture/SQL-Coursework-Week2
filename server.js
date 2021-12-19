4bc9c333cbb676e6901d15c46c1d737d6c4146e6
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-12-19 20:51:56 +0000

    POST endpoint `/customers/:customerId/orders`

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
-const res = require("express/lib/response");
+// const bodyParser = require("body-parser");
+// const res = require("express/lib/response");
 const app = express();
 app.use(express.json());
 app.use(cors());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//   })
+// );
+// app.use(bodyParser.json());
+
 
 const pool = new Pool({
   user: "postgres",
@@ -33,23 +41,22 @@ app.get("/suppliers", (req, res) => {
 // GET all the product names along with their prices and supplier names.
 app.get("/products", (req, res) => {
   const name = req.query.name;
-  console.log(name)
+  console.log(name);
   const query =
     "SELECT p.product_name, pa.unit_price, s.supplier_name FROM products p inner join product_availability pa on pa.prod_id = p.id inner join suppliers s on s.id = pa.supp_id WHERE product_name = $1";
-   if (!name) {
-     pool
-       .query("SELECT * FROM products")
-       .then((result) => res.send(result.rows))
-       .catch((e) => console.error(e));
-   }else{
+  if (!name) {
+    pool
+      .query("SELECT * FROM products")
+      .then((result) => res.send(result.rows))
+      .catch((e) => console.error(e));
+  } else {
     pool
       .query(query, [name])
       .then((result) => {
-       res.send(result.rows)
+        res.send(result.rows);
       })
       .catch((e) => console.error(e));
-   }
-    
+  }
 });
 
 // GET customers by Id
@@ -104,38 +111,82 @@ app.post("/products", (req, res) => {
   }
 });
 
-// post to create a new product availability
-app.post('/availability', (req,res) => {
- const newProdId = req.body.prod_id;
- const newSuppId = req.body.supp_id;
- const newUnitPrice = req.body.unit_price; 
+// post endpoint `/availability` to create a new product availability
+app.post("/availability", (req, res) => {
+  const newProdId = req.body.prod_id;
+  const newSuppId = req.body.supp_id;
+  const newUnitPrice = req.body.unit_price;
 
+  const query =
+    "INSERT INTO product_availability (prod_id, supp_id, unit_price) VALUES ($1, $2, $3)";
+  pool
+  .query('SELECT * FROM products p WHERE id = $1', [newProdId])
+  .then(result => {
+   if (result.rows.length === 0) {
+     res.status(400).send("The product ID is not existed in products table");
+   } else {
+     pool
+       .query("SELECT * FROM suppliers s WHERE id = $1", [newSuppId])
+       .then((result) => {
+         if (result.rows.length === 0) {
+           res
+             .status(400)
+             .send("The supplier ID is not existed in suppliers table");
+         } else {
+           pool
+             .query(
+               "SELECT * FROM product_availability WHERE prod_id = $1 AND supp_id = $2",
+               [newProdId, newSuppId]
+             )
+             .then((result) => {
+               // console.log(result.rows)
+               if (result.rows.length > 0) {
+                 res
+                   .status(400)
+                   .send("The product ID is existed with the supplier ID");
+               } else if (newUnitPrice < 0) {
+                 res.status(400).send("The price should be positive number");
+               } else {
+                 pool
+                   .query(query, [newProdId, newSuppId, newUnitPrice])
+                   .then(() =>
+                     res
+                       .status(201)
+                       .send("The Product Availability has been updated")
+                   )
+                   .catch((e) => console.error(e));
+               }
+             })
+             .catch((e) => console.error(e));
+         }
+       })
+       .catch((e) => console.error(e));
+   }
+  })
+  .catch(e => console.error(e))
+});
 
- const query =
-   "INSERT INTO product_availability (prod_id, supp_id, unit_price) VALUES ($1, $2, $3)";
+// post endpoint `/customers/:customerId/orders` to create a new order
+app.post('/customers/:customerId/orders', (req, res) => {
+ const customerId = req.params.customerId;
+ const newOrderDate = req.body.order_date;
+ const newOrderReference = req.body.order_reference;
+ const query = "INSERT INTO orders (order_date, order_reference, customer_id) VALUES ($1, $2, $3)";
+ pool
+ .query(`SELECT * FROM customers WHERE id = ${customerId}`)
+ .then(result => {
+  if (result.rows.length === 0){
+   res.status(400).send('The customer ID is NOT existed in customers table.')
+  }else{
+   pool
+   .query(query, [newOrderDate, newOrderReference, customerId])
+   .then(() => res.status(201).send('The order has been UPDATED'))
+   .catch(e => console.error(e))
+  }
+ })
+ .catch(e => console.error(e))
+});
 
- // pool
- //   .query(
- //     `SELECT * FROM product_availability pa inner join products p on p.id = pa.prod_id inner join suppliers s on s.id = pa.supp_id WHERE p.id = ${newProdId} AND s.id = ${newSuppId}`
- //   )
- //   .then((result) => {
- //     if (result.rows.length > 0) {
-       pool
-         .query(query, [newProdId, newSuppId, newUnitPrice])
-         .then(() =>
-           res.status(201).send("The Product Availability has been updated")
-         )
-         .catch((e) => console.error(e));
-     // } else {
-     //   res
-     //     .status(400)
-     //     .send(
-     //       "The product_id or supplier_id is not found in the products or suppliers table"
-     //     );
-     // }
-   // })
-   // .catch((e) => console.error(e));
-})
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
