4aa15df8d7ff781bcb7dbb4e7320da045ef00c6f
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-12-21 09:53:34 +0000

    - Add a new PUT endpoint `/customers/:customerId` to update an existing customer (name, address, city and country)

--- server.js
+++ server.js
@@ -197,101 +197,145 @@ app.put("/customers/:customerId", (req, res) => {
   const city = req.body.city;
   const country = req.body.country;
   console.log(req.body);
-  const query = `UPDATE customers SET name=$1, address=$2, city=$3, country=$4 WHERE id=$5`;
-  // pool
-  //   .query(query, [name, address, city, country, customerId])
-  //   .then(() => res.status(204).send(`The customer ${name} has been updated`))
-  //   .catch((e) => console.error(e));
-  if (name != undefined) {
-    pool
-      .query(`UPDATE customers SET name=$1 WHERE id=$2`, [name, customerId])
-      .then(() => res.status(204))
-      .catch((e) => console.error(e));
-  } else {
-    console.log(name);
-  }
-  if (address != undefined) {
-    pool
-      .query(`UPDATE customers SET address=$1 WHERE id=$2`, [
-        address,
-        customerId,
-      ])
-      .then(() => res.status(204))
-      .catch((e) => console.error(e));
-  } else {
-    console.log(address);
-  }
-  if (city != undefined) {
-    pool
-      .query(`UPDATE customers SET city=$1 WHERE id=$2`, [city, customerId])
-      .then(() => res.status(204))
-      .catch((e) => console.error(e));
-  } else {
-    console.log(city);
-  }
-  if (country != undefined) {
-    pool
-      .query(`UPDATE customers SET country=$1 WHERE id=$2`, [
-        country,
-        customerId,
-      ])
-      .then(() => res.status(204))
-      .catch((e) => console.error(e));
-  } else {
-    console.log(city);
-  }
+  // const query = `UPDATE customers SET name=$1, address=$2, city=$3, country=$4 WHERE id=$5`;
+
+  pool
+    .query(`SELECT * FROM customers WHERE id=${customerId}`)
+    .then((result) => {
+      // update name
+      if (name !== undefined) {
+        pool
+          .query(`UPDATE customers SET name=$1 WHERE id=$2`, [name, customerId])
+          // .then(() => res.status(204))
+          .catch((e) => console.error(e));
+      } else {
+        pool
+          .query(`UPDATE customers SET name=$1 WHERE id=$2`, [
+            result.rows[0].name,
+            customerId,
+          ])
+          // .then(() => res.status(204))
+          .catch((e) => console.error(e));
+      }
+      // address
+      if (address !== undefined) {
+        pool
+          .query(`UPDATE customers SET address=$1 WHERE id=$2`, [
+            address,
+            customerId,
+          ])
+          // .then(() => res.status(204))
+          .catch((e) => console.error(e));
+      } else {
+        pool
+          .query(`UPDATE customers SET address=$1 WHERE id=$2`, [
+            result.rows[0].address,
+            customerId,
+          ])
+          // .then(() => res.status(204))
+          .catch((e) => console.error(e));
+      }
+      // city
+      if (city !== undefined) {
+        pool
+          .query(`UPDATE customers SET city=$1 WHERE id=$2`, [city, customerId])
+          // .then(() => res.status(204))
+          .catch((e) => console.error(e));
+      } else {
+        pool
+          .query(`UPDATE customers SET city=$1 WHERE id=$2`, [
+            result.rows[0].city,
+            customerId,
+          ])
+          // .then(() => res.status(204))
+          .catch((e) => console.error(e));
+      }
+      // country
+      if (country !== undefined) {
+        pool
+          .query(`UPDATE customers SET country=$1 WHERE id=$2`, [
+            country,
+            customerId,
+          ])
+          // .then(() => res.status(204))
+          .catch((e) => console.error(e));
+      } else {
+        pool
+          .query(`UPDATE customers SET country=$1 WHERE id=$2`, [
+            result.rows[0].country,
+            customerId,
+          ])
+          // .then(() => res.status(204))
+          .catch((e) => console.error(e));
+      }
+
+      res.status(201).send("UPDATED")
+    })
+    .catch((e) => console.error(e));
+
+  
+  
 });
 
 // DELETE endpoint `/orders/:orderId` to delete an existing order with customerID in customers table
 
 app.delete(`/orders/:orderId`, (req, res) => {
   const orderId = req.params.orderId;
-pool
-  .query(`DELETE FROM order_items WHERE order_id=${orderId}`)
-  .then(() => {
-    pool
-      .query(`DELETE FROM orders WHERE id=${orderId}`)
-      .then(() => {
-        res.status(201).send("The order has been deleted in the orders table");
-      })
-      .catch((e) => console.error(e));
-  })
-  .catch((e) => console.error(e));
-  
+  pool
+    .query(`DELETE FROM order_items WHERE order_id=${orderId}`)
+    .then(() => {
+      pool
+        .query(`DELETE FROM orders WHERE id=${orderId}`)
+        .then(() => {
+          res
+            .status(201)
+            .send("The order has been deleted in the orders table");
+        })
+        .catch((e) => console.error(e));
+    })
+    .catch((e) => console.error(e));
 });
 
 // DELETE endpoint `/customers/:customerId` to delete an existing customer only if this customer doesn't have orders.
 app.delete(`/customers/:customerId`, (req, res) => {
- const customerId = req.params.customerId;
+  const customerId = req.params.customerId;
 
- pool
- .query(`SELECT * FROM orders WHERE customer_id=${customerId}`)
- .then((result) => {
-  if(result.rows.length === 0){
-   pool
-   .query(`DELETE FROM customers WHERE id=${customerId}`)
-   .then(() => res.status(201).send("The customer does not have an order, so it has been deleted"))
-   .catch(e => console.error(e))
-  }else{
-   res.status(400).send("The customer has an order, so it cannot be deleted")
-  }
- })
- .catch(e => console.error(e))
+  pool
+    .query(`SELECT * FROM orders WHERE customer_id=${customerId}`)
+    .then((result) => {
+      if (result.rows.length === 0) {
+        pool
+          .query(`DELETE FROM customers WHERE id=${customerId}`)
+          .then(() =>
+            res
+              .status(201)
+              .send(
+                "The customer does not have an order, so it has been deleted"
+              )
+          )
+          .catch((e) => console.error(e));
+      } else {
+        res
+          .status(400)
+          .send("The customer has an order, so it cannot be deleted");
+      }
+    })
+    .catch((e) => console.error(e));
 });
 
 // GET endpoint `/customers/:customerId/orders` to load all the orders along with the items in the orders of a specific customer
 app.get(`/customers/:customerId/orders`, (req, res) => {
- const customerId = req.params.customerId;
+  const customerId = req.params.customerId;
 
- const query =
-   "SELECT o.order_date, o.order_reference, p.product_name, pa.unit_price, s.supplier_name, oi.quantity From orders o inner join order_items oi on oi.order_id = o.id inner join products p on oi.product_id = p.id inner join suppliers s on s.id = oi.supplier_id inner join product_availability pa on pa.prod_id = p.id WHERE customer_id=$1";
+  const query =
+    "SELECT o.order_date, o.order_reference, p.product_name, pa.unit_price, s.supplier_name, oi.quantity From orders o inner join order_items oi on oi.order_id = o.id inner join products p on oi.product_id = p.id inner join suppliers s on s.id = oi.supplier_id inner join product_availability pa on pa.prod_id = p.id WHERE customer_id=$1";
 
-   pool
-   .query(query, [customerId])
-   .then ((result) => {
-    res.send(result.rows)
-   })
-   .catch(e => console.error(e))
+  pool
+    .query(query, [customerId])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((e) => console.error(e));
 });
 
 app.listen(3000, function () {
