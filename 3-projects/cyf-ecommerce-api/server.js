2abaf38994e8570927d9d27582acf476c2fc7e05
haseeb3927 <haseeb3927@gmail.com>
2022-02-07 20:44:01 +0000

    PROJECT cyf-ecommerce-api Completed
    Could not make .env and .gitignore work that's why I have removed my database username and password for security reasons.

---
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+app.use(express.json());
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  //   USERNAME and PASSWORD removed for security reasons
+  user: "",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "",
+  port: 5432,
+});
+
+// GET ALL THE CUSTOMERS
+app.get("/customers", function (req, res) {
+  pool
+    .query("SELECT * FROM customers")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+// GET ALL THE SUPPLIERS
+app.get("/suppliers", function (req, res) {
+  pool
+    .query("SELECT * FROM suppliers")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+// ADVANCED LEVEL
+app.get("/products", function (req, res) {
+  pool
+    .query(
+      `SELECT products.product_name AS product, product_availability.unit_price AS price, suppliers.supplier_name AS supplier
+        FROM product_availability
+        JOIN products
+        ON products.id = product_availability.prod_id
+        JOIN suppliers
+        ON suppliers.id = product_availability.supp_id`
+    )
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
