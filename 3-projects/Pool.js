07e38f09e9707fd0b12d368064c8c9b111964b7a
Wiam Nasr <wiamnasr@gmail.com>
2022-02-10 11:35:09 +0000

    adding my work from my repo to track them here under my brach

--- 3-projects/Pool.js
+++ 3-projects/Pool.js
@@ -1,10 +1,12 @@
 const { Pool } = require("pg");
 
+require("dotenv").config();
+
 const pool = new Pool({
   host: "localhost",
   port: 5432,
-  user: "wiam",
-  password: "Hello123",
+  user: `${process.env.USER}`,
+  password: `${process.env.PASSWORD}`,
   database: "cyf_ecommerce",
 });
 
