bf4a13445f659c86d73092db015a7c6ec6143314
Harsheek Thanki <harsheekthanki@gmail.com>
2022-06-14 21:17:56 +0100

    suppliers end path added

--- 3-projects/server.js
+++ 3-projects/server.js
@@ -22,6 +22,16 @@ app.get("/customers", function (req, res) {
     });
 });
 
+app.get("/suppliers", function (req, res) {
+  pool
+    .query("SELECT * FROM suppliers;")
+    .then((result) => res.json(result))
+    .catch((error) => {
+      console.log(error);
+      res.status(500), json(error);
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`Hello my service is running on port ${PORT}`);
 });
