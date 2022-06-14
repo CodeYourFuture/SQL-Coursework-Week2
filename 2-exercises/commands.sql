287451df9557c376953054b8350f1f02f5d40351
Harsheek Thanki <harsheekthanki@gmail.com>
2022-06-14 19:47:43 +0100

    Question 8 complete

--- 2-exercises/commands.sql
+++ 2-exercises/commands.sql
@@ -24,12 +24,42 @@ order by product_availability.unit_price DESC limit 5;
 
 -- 6. Retrieve all the products with their corresponding suppliers. The result should only contain the columns `product_name`, `unit_price` and `supplier_name`
 
-
+select products.product_name, product_availability.unit_price, suppliers.supplier_name from products
+inner join product_availability on products.id = product_availability.prod_id
+inner join suppliers on product_availability.supp_id = suppliers.id;
 
 -- 7. Retrieve all the products sold by suppliers based in the United Kingdom. The result should only contain the columns `product_name` and `supplier_name`.
+
+select products.product_name, suppliers.supplier_name from products
+inner join product_availability on products.id = product_availability.prod_id
+inner join suppliers on product_availability.supp_id = suppliers.id
+where suppliers.country = 'United Kingdom';
+
 -- 8. Retrieve all orders, including order items, from customer ID `1`. Include order id, reference, date and total cost (calculated as quantity * unit price).
+
+select orders.id, products.product_name, orders.order_reference, orders.order_date, product_availability.unit_price * order_items.quantity as total_cost from orders
+inner join customers on customers.id = orders.customer_id
+inner join order_items on orders.id = order_items.order_id
+inner join product_availability on order_items.product_id = product_availability.prod_id
+inner join products on products.id = product_availability.prod_id
+where customers.id = 1;
+
+
+
 -- 9. Retrieve all orders, including order items, from customer named `Hope Crosby`
+
+
 -- 10. Retrieve all the products in the order `ORD006`. The result should only contain the columns `product_name`, `unit_price` and `quantity`.
+
 -- 11. Retrieve all the products with their supplier for all orders of all customers. The result should only contain the columns `name` (from customer), `order_reference`, `order_date`, `product_name`, `supplier_name` and `quantity`.
+
 -- 12. Retrieve the names of all customers who bought a product from a supplier based in China.
+
+-- select customers.name from customers
+-- inner join orders on customers.id = orders.customer_id
+-- inner join order_items on orders.id = order_items.order_id
+-- inner join suppliers on suppliers.id = product_availability.supp_id
+-- inner join product_availability on 
+-- where suppliers.country = 'China';
+
 -- 13. List all orders giving customer name, order reference, order date and order total amount (quantity * unit price) in descending order of total.
