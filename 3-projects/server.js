240acea007883d72cf091136758ae3a1e88c64b4
Harsheek Thanki <harsheekthanki@gmail.com>
2022-06-14 20:55:08 +0100

    server.js file added to projects folder

---
+++ 3-projects/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+// const express = require("express");
+// const app = express();
+
+// const PORT = 9001;
+
+// const data = [
+//   { id: 1, name: "Harsheek", age: 35 },
+//   { id: 2, name: "Helen", age: 30 },
+//   { id: 3, name: "Jack", age: 25 },
+// ];
+
+// app.get("/users/:id", (req, res) => {
+//   const user = data.find((u) => u.id === Number(req.params.id));
+
+//   if (user) return res.send(user);
+//   res.status(404).send("Whoops, not here sorry");
+// });
+
+// app.get("/users", (req, res) => {
+//   res.send(data);
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Hello my service is running on ${PORT}`);
+// });
