7bc6cfe05ac7c53dc9310ba3386f769c6a4d313a
Harsheek Thanki <harsheekthanki@gmail.com>
2022-06-14 21:28:01 +0100

    products end path added to projects folder

--- 3-projects/server.js
+++ 3-projects/server.js
@@ -34,7 +34,9 @@ app.get("/suppliers", function (req, res) {
 
 app.get("/products", function (req, res) {
   pool
-    .query("SELECT * FROM suppliers;")
+    .query(
+      "select products.product_name, product_availability.unit_price, suppliers.supplier_name from products inner join product_availability on products.id = product_availability.prod_id inner join suppliers on suppliers.id = product_availability.supp_id;"
+    )
     .then((result) => res.json(result))
     .catch((error) => {
       console.log(error);
