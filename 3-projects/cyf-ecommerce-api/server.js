9526f10930794dfe69d48d62c6e2fcdf8a380467
hachi-ops <elen20@protonmail.com>
2022-06-18 23:54:01 +0100

    push

--- 3-projects/cyf-ecommerce-api/server.js
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -1,11 +1,55 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const PORT = 3000;
+const { Pool } = require("pg");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const pool = new Pool({
+  user: "helen",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "01234",
+  port: 5678,
 });
 
-app.listen(port, () => {
-  console.log(`This app listening on port ${port}`);
+app.get("/", (req, res) => {
+  res.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+const getCustomers = (req, res) => {
+  return pool
+    .query("SELECT * FROM customers")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+};
+
+const getSuppliers = (req, res) => {
+  return pool
+    .query("SELECT * FROM suppliers")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+};
+
+const getProducts = (req, res) => {
+  return pool
+    .query(
+      "SELECT products.product_name, product_availability.unit_price, suppliers.supplier_name FROM product_availability INNER JOIN products ON products.id=product_availability.prod_id LEFT JOIN suppliers ON suppliers.id=product_availability.supp_id"
+    )
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+};
+app.get("/customers", getCustomers);
+app.get("/suppliers", getSuppliers);
+app.get("/products", getProducts);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`);
 });
