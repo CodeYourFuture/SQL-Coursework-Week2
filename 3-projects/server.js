41d2870857d6ce9beff93b4819e1d4ec15a36c3d
MatildaAko <matildaako@gmail.com>
2022-06-21 17:32:07 +0100

    Started week 3

--- 3-projects/server.js
+++ 3-projects/server.js
@@ -1,7 +1,3 @@
-/* Add a new GET endpoint `/customers` to return all the customers from the database
-- Add a new GET endpoint `/suppliers` to return all the suppliers from the database
-- (STRETCH GOAL) Add a new GET endpoint `/products` to return all the product names along with their prices and supplier names.
- */
 const express = require("express");
 const { Pool } = require("pg");
 
@@ -39,13 +35,31 @@ app.get("/suppliers", (req, res) => {
       res.status(500).json(error);
     });
 });
-
+const productsQuery =
+  'SELECT products.product_name as "productName", unit_price as price, suppliers.supplier_name as "supplier" FROM product_availability INNER JOIN products ON products.id = product_availability.prod_id INNER JOIN suppliers ON suppliers.id = product_availability.supp_id;';
 app.get("/products", (req, res) => {
   pool
-    .query(
-      'SELECT products.product_name as "productName", unit_price as price, suppliers.supplier_name as "supplier" FROM product_availability INNER JOIN products ON products.id = product_availability.prod_id INNER JOIN suppliers ON suppliers.id = product_availability.supp_id;'
-    )
+    .query(productsQuery)
     .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.post("/products", (req, res) => {
+  const newProductName = req.body.product_name;
+  const newUnitPrice = req.body.unit_price;
+  const newSupplierRooms = req.body.supplier_name;
+  const query = "INSERT INTO products (product_name) VALUES ($1) INSERT INTO suppliers(supplier_name) VALUES ($2) INSERT INTO product_availability VALUES ($3)";
+
+  if (!Number.isInteger(newUnitPrice) || newUnitPrice <= 0) {
+    return res.status(400).send("The product price should be a positive integer.");
+  }
+
+  pool
+    .query(query, [newProductName, newUnitPrice, newSupplierRooms])
+    .then(() => res.send("Product created!"))
     .catch((error) => {
       console.error(error);
       res.status(500).json(error);
