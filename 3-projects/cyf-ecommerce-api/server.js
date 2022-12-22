f5e426b2211b045816d334552cc568089bd3a18e
Riyaaz Singh <riyaazct1@gmail.com>
2022-12-22 20:53:42 +0200

    update server.js

---
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "riyaaz",
+  host: "dpg-cefmi99gp3jk7mhomnhg-a.oregon-postgres.render.com",
+  database: "cyf_ecommerce_i43p",
+  password: "m35aBThruz4miC9Ak7Hsjdwgwy5qlxBt",
+  port: 5432,
+  ssl: { rejectUnauthorized: false },
+});
+
+app.get("/customers", (req, res) => {
+  pool
+    .query("SELECT * from customers c")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.listen(port, function () {
+  console.log(
+    `Server is listening on port ${port}. Ready to accept requests!`
+  );
+});
