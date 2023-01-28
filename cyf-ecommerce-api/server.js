4cc50175639905c9e1c03836365e9ad961e731a6
jaspreet kaur <kaurjas1928@yahoo.com>
2023-01-28 15:22:02 +0000

    added new end point products

--- cyf-ecommerce-api/server.js
+++ cyf-ecommerce-api/server.js
@@ -23,6 +23,28 @@ app.get("/customers", (req, res) => {
     });
 });
 
+app.get("/suppliers", (req, res) => {
+  pool
+    .query("SELECT * FROM suppliers")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.get("/products", (req, res) => {
+  pool
+    .query(
+      "SELECT products.product_name, product_availability.unit_price,suppliers.supplier_name from product_availability inner join products on products.id=product_availability.prod_id inner join suppliers on suppliers.id=product_availability.supp_id"
+    )
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
