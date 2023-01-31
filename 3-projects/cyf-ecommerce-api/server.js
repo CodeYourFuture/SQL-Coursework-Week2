cd7b4c0c5b84d95a327ad3163a23ef783877b034
vanipriya_piramanathan <vpriyaarun23@gmail.com>
2023-01-31 13:58:00 +0000

    exercise and project completed

--- 3-projects/cyf-ecommerce-api/server.js
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const { Pool } = require("pg");
 
 const db = new Pool({
-  user: "arunk",
+  user: process.env.DB_USER,
   host: "localhost",
   database: "cyf_ecommerce",
-  password: "",
+  password: process.env.DB_PASSWORD,
   port: 5432,
 });
 
@@ -31,7 +33,10 @@ app.get("/suppliers", function (req, res) {
 
 app.get("/products", function (req, res) {
   db.query(
-    "SELECT p.product_name, pa.unit_price, s.supplier_name FROM products AS p JOIN product_availability AS pa ON p.id = pa.prod_id JOIN suppliers AS s ON pa.supp_id = s.id"
+    `SELECT p.product_name, pa.unit_price, s.supplier_name 
+    FROM products AS p 
+    JOIN product_availability AS pa ON p.id = pa.prod_id 
+    JOIN suppliers AS s ON pa.supp_id = s.id`
   )
     .then((result) => res.json(result.rows))
     .catch((error) => {
