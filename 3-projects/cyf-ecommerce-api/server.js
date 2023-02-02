108f784081c684bc2e365854a1fe89b39ed973d0
SaadiaELF <saadiaelfekak@gmail.com>
2023-02-02 02:15:36 +0000

    added get all customers route

--- 3-projects/cyf-ecommerce-api/server.js
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "",
+  port: 5432,
+});
 
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
+});
+
+// Get all customers
+app.get("/customers", async function (req, res) {
+  try {
+    let result = await pool.query("SELECT * FROM customers");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
