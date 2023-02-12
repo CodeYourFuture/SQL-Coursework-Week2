20f9fcc4c1cabeb0afeac65f30676ec0a0d479bc
khmdagal <khmdagal@gmail.com>
2023-02-12 22:40:24 +0000

    Create yf-ecommerce-api

---
+++ 3-projects/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const port = 500;
+
+app.use(express.json());
+
+const { Pool } = require("");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "Kdagaal123",
+  port: 5432,
+});
+
+app.get("cyf-ecommerce-api", (req, res) => {
+  res.status(200).send(`Wellcom cyf-ecommerce-api
+    visit these roots;
+/customers
+/suppliers
+/products
+    `);
+});
+
+app.get("cyf-ecommerce-api/customers", (req, res) => {
+  pool
+    .query("SELECT * FROM customers")
+    .then((data) => res.json(data.rows))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+});
+
+app.get("cyf-ecommerce-api/suppliers", (req, res) => {
+  pool
+    .query("SELECT * FROM suppliers")
+    .then((data) => res.json(data.rows))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+});
+
+app.get("cyf-ecommerce-api/products", (req, res) => {
+  pool
+    .query("SELECT * FROM products")
+    .then((data) => res.json(data.rows))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+});
+app.listen(port, () => {
+  console.log(`server is running ${port}`);
+});
