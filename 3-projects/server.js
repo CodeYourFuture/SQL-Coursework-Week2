45a466328c7e4f29277ec9ffe855ebb4c90011f1
fatih-celebi <celebifatih.c1@gmail.com>
2023-02-13 00:01:20 +0000

    Complete Extra Challenge

--- 3-projects/server.js
+++ 3-projects/server.js
@@ -21,6 +21,14 @@ app.get("/suppliers", (req, res) => {
     res.json(result.rows)
   })
 })
+
+app.get("/products", (req, res) => {
+  db.query(
+    "select p.product_name, p_a.unit_price, s.supplier_name from products p inner join product_availability p_a on p.id = p_a.prod_id inner join suppliers s on p_a.supp_id = s.id", (err, result) => {
+      res.json(result.rows);
+    });
+});
+
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
