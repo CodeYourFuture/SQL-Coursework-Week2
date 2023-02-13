8b9f9ab8bc3520b49d6be0038bbd9db557f496e8
khmdagal <khmdagal@gmail.com>
2023-02-13 00:18:19 +0000

    Fix The / route

--- 3-projects/server.js
+++ 3-projects/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 500;
+const port = 5000;
 
 app.use(express.json());
 
-const { Pool } = require("");
+const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
@@ -14,7 +14,8 @@ const pool = new Pool({
   port: 5432,
 });
 
-app.get("cyf-ecommerce-api", (req, res) => {
+
+app.get("/cyf-ecommerce-api", (req, res) => {
   res.status(200).send(`Wellcom cyf-ecommerce-api
     visit these roots;
 /customers
@@ -23,18 +24,17 @@ app.get("cyf-ecommerce-api", (req, res) => {
     `);
 });
 
-app.get("cyf-ecommerce-api/customers", (req, res) => {
-  // pool
-  //   .query("SELECT * FROM customers")
-  //   .then((data) => res.json(data.rows))
-  //   .catch((err) => {
-  //     console.error(err);
-  //     res.status(500).json(err);
-  //   });
-  res.send('hello')
+app.get("/cyf-ecommerce-api/customers", (req, res) => {
+  pool
+    .query("SELECT * FROM customers")
+    .then((data) => res.json(data.rows))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
 });
 
-app.get("cyf-ecommerce-api/suppliers", (req, res) => {
+app.get("/cyf-ecommerce-api/suppliers", (req, res) => {
   pool
     .query("SELECT * FROM suppliers")
     .then((data) => res.json(data.rows))
@@ -44,7 +44,7 @@ app.get("cyf-ecommerce-api/suppliers", (req, res) => {
     });
 });
 
-app.get("cyf-ecommerce-api/products", (req, res) => {
+app.get("/cyf-ecommerce-api/products", (req, res) => {
   pool
     .query("SELECT * FROM products")
     .then((data) => res.json(data.rows))
@@ -53,6 +53,8 @@ app.get("cyf-ecommerce-api/products", (req, res) => {
       res.status(500).json(err);
     });
 });
+
 app.listen(port, () => {
   console.log(`server is running ${port}`);
 });
+ 
