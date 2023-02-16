5b7a51ad8f88f8f220342dbd92ae62408aaee6ce
Anosh Malik <anoshmalik.am@gmail.com>
2023-02-16 11:07:33 +0000

    Added more endpoints according to SQL W3. Not functional yet

--- cyf-ecommerce-api/server.js
+++ cyf-ecommerce-api/server.js
@@ -20,6 +20,12 @@ app.get("/customers", function (req, res) {
   });
 });
 
+// SQL W3
+app.get("/customers", function (req, res) {
+  db.query("SELECT * FROM customers", (error, result) => {
+    res.json(result.rows);
+  });
+});
 
 app.get("/suppliers", function (req, res) {
   db.query("SELECT * FROM suppliers", (error, result) => {
@@ -32,4 +38,77 @@ app.get("/products", function (req, res) {
   db.query("SELECT product_availability.unit_price, products.product_name, suppliers.supplier_name FROM product_availability INNER JOIN products on products.id=product_availability.prod_id INNER JOIN suppliers ON suppliers.id=product_availability.supp_id", (error, result) => {
     res.json(result.rows);
   });
+});
+
+
+app.get(`/customers/:customerId/orders`, function (req, res) {
+  db.query(
+    "SELECT product_availability.unit_price, products.product_name, suppliers.supplier_name FROM product_availability INNER JOIN products on products.id=product_availability.prod_id INNER JOIN suppliers ON suppliers.id=product_availability.supp_id",
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
+});
+
+app.post("/customers", function (req, res) {
+  db.query(
+    "SELECT product_availability.unit_price, products.product_name, suppliers.supplier_name FROM product_availability INNER JOIN products on products.id=product_availability.prod_id INNER JOIN suppliers ON suppliers.id=product_availability.supp_id",
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
+});
+
+app.post("/products", function (req, res) {
+  db.query(
+    "SELECT product_availability.unit_price, products.product_name, suppliers.supplier_name FROM product_availability INNER JOIN products on products.id=product_availability.prod_id INNER JOIN suppliers ON suppliers.id=product_availability.supp_id",
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
+});
+
+app.post("/availability", function (req, res) {
+  db.query(
+    "SELECT product_availability.unit_price, products.product_name, suppliers.supplier_name FROM product_availability INNER JOIN products on products.id=product_availability.prod_id INNER JOIN suppliers ON suppliers.id=product_availability.supp_id",
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
+});
+
+app.post(`/customers/:customerId/orders`, function (req, res) {
+  db.query(
+    "SELECT product_availability.unit_price, products.product_name, suppliers.supplier_name FROM product_availability INNER JOIN products on products.id=product_availability.prod_id INNER JOIN suppliers ON suppliers.id=product_availability.supp_id",
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
+});
+
+app.put(`/customers/:customerId/orders`, function (req, res) {
+  db.query(
+    "SELECT product_availability.unit_price, products.product_name, suppliers.supplier_name FROM product_availability INNER JOIN products on products.id=product_availability.prod_id INNER JOIN suppliers ON suppliers.id=product_availability.supp_id",
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
+});
+
+app.delete(`/customers/:customerId/orders`, function (req, res) {
+  db.query(
+    "SELECT product_availability.unit_price, products.product_name, suppliers.supplier_name FROM product_availability INNER JOIN products on products.id=product_availability.prod_id INNER JOIN suppliers ON suppliers.id=product_availability.supp_id",
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
+});
+
+app.delete(`/orders/:orderId`, function (req, res) {
+  db.query(
+    "SELECT product_availability.unit_price, products.product_name, suppliers.supplier_name FROM product_availability INNER JOIN products on products.id=product_availability.prod_id INNER JOIN suppliers ON suppliers.id=product_availability.supp_id",
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
 });
