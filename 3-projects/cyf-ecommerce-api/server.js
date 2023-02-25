741eeac55c1f7ccdbc7d2ae4a0c0e800b1f66f91
migue <miguelanjo2@hotmail.com>
2023-02-25 11:47:42 +0000

    update product endpoint

--- 3-projects/cyf-ecommerce-api/server.js
+++ 3-projects/cyf-ecommerce-api/server.js
@@ -51,7 +51,12 @@ app.get("/suppliers", function(req, res){
 
 //Add a new GET endpoint `/products`
 app.get("/products", function(req, res){
-  pool.query("SELECT * FROM products ")
+  pool.query(`SELECT products.product_name, product_availability.unit_price, suppliers.supplier_name
+  FROM products
+  INNER JOIN product_availability
+  ON products.id = product_availability.prod_id
+  INNER JOIN suppliers
+  ON product_availability.supp_id = suppliers.id;`)
   .then((result) => res.json(result.rows))
       .catch((error) => {
           console.error(error);
